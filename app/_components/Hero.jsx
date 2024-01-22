29a489e9b965b6b430850f77222f570f456842ab
zeeshanahme-d <ka265923@gmail.com>
2024-01-22 23:19:40 +0500

    add style

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -20,14 +20,14 @@ function Hero() {
 
                     <div className="mt-8 flex flex-wrap justify-center gap-4">
                         <Link
-                            className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                            className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 sm:w-auto"
                             href="/files"
                         >
                             Get Started
                         </Link>
 
                         <a
-                            className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-blue-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                            className="block w-full rounded px-12 py-3 text-sm font-medium text-primary shadow hover:text-blue-700  sm:w-auto"
                             href="/about"
                         >
                             Learn More
