bf189c80cfc14edf9effc745f2886e0d16f10d1c
Zeeshan Ahmed <dev.zeeshanahmed@gmail.com>
2025-04-24 07:09:58 -0700

    fix: update footer links to correct GitHub and LinkedIn profiles

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
     return (
         <div className='w-full absolute bottom-2 mt-2 flex-col flex items-center gap-3'>
             <div className='text-sm lg:text-base'>
-            @2024 Created by Zeeshan Ahmed
+                @2024 Created by Zeeshan Ahmed
             </div>
             <div className='flex gap-2'>
-                <Link href='https://github.com/zeeshan-ahmed-smit'>
+                <Link href='https://github.com/zeeshanahme-d'>
                     <Github className='text-base cursor-pointer' />
                 </Link>
-                <Link href='https://linkedin.com/in/zeeshan-ahmed-smit'>
+                <Link href='https://linkedin.com/in/zeeshanahme-d'>
                     <Linkedin className='text-base cursor-pointer' />
                 </Link>
             </div>
